03960241dd6fb5301eed4910227d7d0f6ce4e4b0
villelinhalaura <106742746+villelinhalaura@users.noreply.github.com>
2023-10-27 09:01:49 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-DocumentTimeline.querySelector('.teclado')
+functiontocaSomPom(){
+document.querySelector('#som_tecla_pom').play(''):
+
+}
+const listaDeTeclas = document.querySelectorAll('.tecla')
+
+let contador = 0:
+
+while(contador < 9){
+    
+    const instrumento = listaDeTeclas[contador].classList[1]
+    
+
+    listaDeTeclas[contador].onclick = function(){
+      tocaSom('#som_tecla-clap');
+    };
+   
+    contador = contador +1;
+}
+    
+listaDeTeclas[1].onclick = tocaSomPom;
+listaDeTeclas [2].onclick = tocaSomTim;
+listaDeTeclas [3].onclick = tocaSomPuff
