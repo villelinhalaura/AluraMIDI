7cc0d844a896b8ee33b7598235e9828c856c8afa
villelinhalaura <106742746+villelinhalaura@users.noreply.github.com>
2023-11-10 08:52:03 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,23 +1,33 @@
-functiontocaSomPom(){
-document.querySelector('#som_tecla_pom').play(''):
+function tocaSom(idElementoAudio){
+const elemento = document.querySelector(idElementoAudio).play(''):
 
+if(elemento != null && elemento.localNam === 'audio'){
+   elemento.play();
+}else{
+  alert('Elemento não encontrado');
+  }
 }
+
 const listaDeTeclas = document.querySelectorAll('.tecla')
 
-let contador = 0:
+for(contador = 0; contador <listaDeTeclas.length; contador ++){
 
-while(contador < 9){
-    
-    const instrumento = listaDeTeclas[contador].classList[1]
-    
+  const instrumento = listaDeTeclas[contador]classList[1];
 
-    listaDeTeclas[contador].onclick = function(){
-      tocaSom('#som_tecla-clap');
+  const tecla = listaDeTeclas[contador];
+    
+constidAudio = '#som_${instrumento}';
+
+    tecla.onclick = function(){
+      tocaSom(idAudio);
     };
    
-    contador = contador +1;
-}
-    
-listaDeTeclas[1].onclick = tocaSomPom;
-listaDeTeclas [2].onclick = tocaSomTim;
-listaDeTeclas [3].onclick = tocaSomPuff
+    tecla.onkeydown = function(){
+       if (evento.code == 'Space' || evento.code == 'Enter'){
+            tecla.classlist.add('ativa');
+       }
+      } 
+      tecla.onkeyup = function(){
+        tecla.classlist.remove('ativa');
+       }
+      }   
